f6997542704d624b8e466a21b223074d0b080147
KOUSTUBH BADSHAH <koustubhbadshah07@outlook.com>
2023-11-07 20:11:48 +0000

    des and railfence first commit not integrated in main

---
+++ source/des.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+class des
+{
+private:
+    string message;
+    int *decimalMessage,*binaryMessage,*tempDecimal;
+public:
+    des();
+    void stringToDecimal();
+    void decimalToBinary();
+    ~des();
+};
+
+des::des(){
+    cout<<"enter message\n";
+    getline(cin,message);
+    stringToDecimal();
+    decimalToBinary();
+    
+}
+
+void des::stringToDecimal(){
+    decimalMessage = new int();
+    for (int i = 0; i < message.length(); i++)
+    {
+        long int val=0;
+        val= int(message[i]);
+        decimalMessage[i]=val;
+    }
+    for (int i = 0; i < message.length(); i++)
+    {
+        cout<<decimalMessage[i]<<" ";
+    }
+    cout<<endl;
+    
+}
+
+void des::decimalToBinary(){
+    binaryMessage = new int(message.length());
+    long int value=0;
+    string dval = ""; 
+    for (int i = 0; i < message.length(); i++)
+    {
+        value=decimalMessage[i];
+        long int dCounter =0;
+        if(value < 16 ){
+            tempDecimal = new int(4);
+        }else if(value > 15  && value < 257 ){
+            tempDecimal = new int(8);
+        }else{
+            tempDecimal = new int(12);
+        }
+        while (value>0)
+        {
+            tempDecimal[dCounter]=value%2;
+            value=value/2;
+            ++dCounter;
+        }
+        
+        for (int i = dCounter - 1; i >= 0; i--)
+        {
+            dval += to_string(tempDecimal[i]);
+        }
+        binaryMessage[i] = stoi(dval,0,10);
+        dval = "";
+
+    }
+    for (int i = 0; i < message.length(); i++)
+    {
+        cout<<binaryMessage[i]<<" ";
+    }
+    cout<<endl;
+    
+    
+}
+
+des::~des()
+{
+    delete decimalMessage,binaryMessage,tempDecimal;
+    cout<<"\n hi\n";
+}
+
+int main(){
+    des o;
+    return 0;
+}
---
+++ source/railfence.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+class railfence{
+private:
+    string encrypted_text;
+    string decrypted_text;
+    public:
+    void railfence_cipher(int depth,string text);
+    void railfence_decipher(int depth,string text);
+};
+
+
+void railfence::railfence_cipher(int depth,string text){
+
+}
+
+void railfence::railfence_decipher(int depth,string text){
+
+}
